203cf94a527e0210e2d7d03fe39f81033df0e9fd
Zaasth <wellintonpaypal@gmail.com>
2024-04-29 22:52:08 -0300

    Variaveis e constantes

---
+++ Estudos Wellinton/src/edu/wellinton/testes/TiposEVariaveis/Variaveis2.java
@@ -0,0 +1,9 @@
+package edu.wellinton.testes.TiposEVariaveis;
+
+public class Variaveis2 {
+    public static void main(String[] args) {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        numeroCurto = (short) numeroNormal;
+    }
+}
